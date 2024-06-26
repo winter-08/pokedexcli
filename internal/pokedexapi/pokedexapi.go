9cff800dc97749ed2f9a618ab7c5a1cf06fc354e
Winter <jonathan.bold@gmail.com>
2024-06-26 19:28:10 +0100

    pkg fixes

--- internal/pokedexapi/pokedexapi.go
+++ internal/pokedexapi/pokedexapi.go
@@ -41,8 +41,97 @@ type Locations struct {
 	} `json:"results"`
 }
 
+type LocationArea struct {
+	ID                   int    `json:"id"`
+	Name                 string `json:"name"`
+	GameIndex            int    `json:"game_index"`
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int `json:"rate"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"encounter_method_rates"`
+	Location struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Names []struct {
+		Name     string `json:"name"`
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+	} `json:"names"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		VersionDetails []struct {
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+			MaxChance        int `json:"max_chance"`
+			EncounterDetails []struct {
+				MinLevel        int   `json:"min_level"`
+				MaxLevel        int   `json:"max_level"`
+				ConditionValues []any `json:"condition_values"`
+				Chance          int   `json:"chance"`
+				Method          struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+			} `json:"encounter_details"`
+		} `json:"version_details"`
+	} `json:"pokemon_encounters"`
+}
+
+func GetLocationArea(name string, cache *pokecache.Cache) (*LocationArea, error) {
+  cachedRes, found := cache.Get(name)
+
+  if found == true {
+    cachedResponse := LocationArea{}
+    json.Unmarshal(*cachedRes, &cachedResponse)
+    return &cachedResponse, nil
+  }
+
+  res, err := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/location-area/%v", name))
+
+  if err != nil {
+    fmt.Printf("There was an error getting location area: %v\n", err)
+    return nil, err
+  }
+
+  body, err := io.ReadAll(res.Body)
+  res.Body.Close()
+
+  if res.StatusCode > 299 {
+    fmt.Printf("res.StatusCode: %v\n", res.StatusCode)
+    fmt.Printf("body: %v\n", body)
+    return nil, errors.New("Error getting location area")
+  }
+
+  cache.Add(name, body)
+  
+  response := LocationArea{}
+  json.Unmarshal(body, &response)
+
+  return &response, nil
+
+}
+
 func GetLocations(url string, cache *pokecache.Cache) (*Locations, error) {
   cachedRes, found := cache.Get(url)
+
+  fmt.Printf("found: %v\n", found)
 
   if found == true {
     cachedResponse := Locations{}
@@ -64,6 +153,8 @@ func GetLocations(url string, cache *pokecache.Cache) (*Locations, error) {
     fmt.Printf("body: %v\n", body)
     return nil, errors.New("Error getting locations")
   }
+
+  cache.Add(url, body)
 
   response := Locations{}
   json.Unmarshal(body, &response)
